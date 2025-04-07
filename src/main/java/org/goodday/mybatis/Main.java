418ee95133527167ae51adfeb6f7957d78004852
zhulianwei80371 <108607248+zhulianwei80371@users.noreply.github.com>
2025-04-07 14:48:08 +0800

    Update Main.java

--- src/main/java/org/goodday/mybatis/Main.java
+++ src/main/java/org/goodday/mybatis/Main.java
@@ -2,7 +2,7 @@ package org.goodday.mybatis;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!1");
     }
     // git初始化
 }
