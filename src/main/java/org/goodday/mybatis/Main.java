6f869c7bf4e1407be28a54d58eedd30c9ec4a64c
lianweizhu <632229677@qq.com>
2025-04-07 14:56:36 +0800

    初始化

--- src/main/java/org/goodday/mybatis/Main.java
+++ src/main/java/org/goodday/mybatis/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!5");
         System.out.println("Hello world!6");
+        System.out.println("Hello world!7");
+        System.out.println("Hello world!8");
     }
     // git初始化
 }
